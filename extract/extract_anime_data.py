71ac91e9dcc82953caa996735bc5291d09f36718
Lamo Kouravand <lamochi02@gmail.com>
2025-03-23 08:37:16 +0100

    all data except genre is included in the dataframe

--- extract/extract_anime_data.py
+++ extract/extract_anime_data.py
@@ -1,54 +1,122 @@
 
 import requests
 import pandas as pd
-import time
-import os
+import emoji
+import re
 
-def extract(anime_id):
+def extract(page_number):
+    """extract a page from jikan API"""
+
+    base_rul = f'https://api.jikan.moe/v4/anime/'
+    url = f'{base_rul}?page={page_number}'
+
+    while True:
+        try:
+            response = requests.get(url)
+
+            if response.status_code == 200:
+                json_data = response.json()
+
+                if not json_data['data']:
+                    break
+
+                else:
+                    return json_data['data']
+
+
+        except Exception as e:
+            print(f'error {e}')
+
+
+
+# additional data cleaning functions
+############################################################
+def remove_emojis(text):
+    return emoji.replace_emoji(text, replace='')
+
+def convert_to_minutes(duration):
+    """convert hours to integers in minutes"""
+
+    if 'hr' in duration and 'min' in duration:
+        hours = re.findall(r'(\d+)\s*hr', duration)
+        minutes = re.findall(r'(\d+)\s*min', duration)
+
+        total_minutes = int(hours[0]) * 60 + int(minutes[0]) if hours and minutes else 0
+        return total_minutes
     
-    url = f"https://api.jikan.moe/v4/anime/{anime_id}/"
-
-    try:
-        response = requests.get(url)
-        if response.status_code == 200:
-            response_data = response.json()
-            normalized_data = pd.json_normalize(response_data['data'])
-            formatted_data = pd.DataFrame(normalized_data)
-            formatted_data['studios_name'] = formatted_data['studios'].apply(lambda x: x[0]['name'] if isinstance(x, list) & len(x) > 0 else None)
-            formatted_data['aired_from'] = formatted_data['aired.from']
-            formatted_data['aired_to'] = formatted_data['aired.to']
-            formatted_data['trailer_url'] = formatted_data['trailer.url']
-            formatted_data['genre'] = formatted_data['genres'].apply(lambda x: x[0]['name'] if isinstance(x, list) & len(x) > 0 else None)
-
-            df = formatted_data[['title_english', 'episodes',
-                                'duration', 'score', 'aired_from',
-                                'aired_to', 'url', 'trailer_url',
-                                'studios_name', 'approved' ]]
-            
-            csv_file_exists = os.path.exists('anime_data.csv')
-            df.to_csv('anime_data.csv', mode='a', index=False, header=not csv_file_exists)
-
-            return print(df)
-        
-        elif response.status_code == 404:
-            error_response = response.json()
-            error_data_status = error_response['status']
-            error_data_message = error_response['message']
-            return print(f'{error_data_status}, Anime Not Found/{error_data_message}')
-        
-        elif response.status_code == 500:
-            return print(f'Internal Server Error. The developers are on it, please try again later.')
-        else:
-            return print(f'something weird went wrong...')
+    elif 'min' in duration:
+        minutes = re.findall(r'(\d+)\s*min', duration)
+        return int(minutes[0]) if minutes else 0
     
-    except Exception as e:
-        print(f'error {e}')
-        return
+    elif 'hr' in duration:
+        hours = re.findall(r'(\d+)\s*hr', duration)
+        return int(hours[0]) * 60 if hours else 0
     
+    else:
+        return 0
+    
+
+def clean_title(text):
+    """clean title of each anime for enhanced query accuracy"""
+
+    text = re.sub(r'[^\x00-\x7F]+', ' ', text)
+    text = remove_emojis(text)                  
+    text = text.lower()                         
+    text = text.replace(',', ' ')               
+    text = text.replace('&', 'and')             
+    text = text.replace('/', ' ')               
+    text = text.replace('☆', ' ')
+
+    return text
+############################################################
+
+
+
+
+def transform(i):
+    """transform to showcase the data based on the KPI """
+
+    data = extract(i)
+    normalized_data = pd.json_normalize(data)
+    
+    
+
+    # pd.isna() checks if the value is none
+    normalized_data.dropna(subset=['title_english', 'title'], how='all', inplace=True) #drop the row if *both* are None 
+    normalized_data['title_english'] = normalized_data.apply(
+        lambda row: row['title'] if pd.isna(row['title_english']) else row['title_english'], axis=1
+    )
+
+    # manipulate the naming convention
+    normalized_data['studio'] = (
+        normalized_data['studios'].apply(
+        lambda studio: studio[0]['name'] if isinstance(studio, list) and len(studio) > 0 else 'None'
+    )
+    .str.lower()
+    )
+    normalized_data[['trailer_link', 'validated', 'title', 'aired_from', 'aired_to']] = \
+    normalized_data[['trailer.url', 'approved', 'title_english', 'aired.from', 'aired.to']]
+    normalized_data['title'] = normalized_data['title'].apply(clean_title) # no lambda because of a row is none, code will drop the entire row
+    
+    # only use the first 10 characters of the string
+    normalized_data[['aired_from', 'aired_to']] = normalized_data[['aired_from', 'aired_to']].apply(lambda x: x.str[:10])
+
+    #handle episodes column data cleaning
+    normalized_data['episodes'] = normalized_data['episodes'].apply(lambda x: 'None' if pd.isna(x) else int(x))
+
+    # clean data in duration column
+    if 'duration' in normalized_data.columns:
+        normalized_data['duration'] = normalized_data['duration'].apply(lambda x: 'None' if pd.isna(x) else convert_to_minutes(x))
+    
+    
+    #all elements in dataframe
+    df = normalized_data[['title', 'aired_from', 'aired_to', 'episodes', 'duration', 'score', 'trailer_link', 'studio', 'validated']]
+    return print(df)
+
+    
+
 if __name__ == "__main__":
     """runs the function locally merely if this file is run"""
 
-    id = 1
-    for i in range(id, 15):
-        extract(i)
-        time.sleep(2)
+    transform(113)
+
